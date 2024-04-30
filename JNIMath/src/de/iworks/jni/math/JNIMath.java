3c85826dc11fbd5c45a3fbe81dc7d179446a119f
gmuel <gabmueller@hotmail.com>
2024-04-30 18:11:21 +0200

    [jni-math] Initial commit JNI test project

---
+++ JNIMath/src/de/iworks/jni/math/JNIMath.java
@@ -0,0 +1,15 @@
+package de.iworks.jni.math;
+
+public class JNIMath {
+
+	/**
+	 * @param x
+	 * @return absolute value of x
+	 */
+	public static native double abs(double x);
+	/**
+	 * @param x
+	 * @return square root of arg 'x'
+	 */
+	public static native double sqrt(double x);
+}
