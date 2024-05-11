fa205199df2fadb1b3db70e29428a7c30cfa0181
gmuel <gabmueller@hotmail.com>
2024-05-11 11:21:06 +0200

    [jni-math] New babel2 added, make correction

--- JNIMathCpp01/lib/impl.cpp
+++ JNIMathCpp01/lib/impl.cpp
@@ -13,16 +13,20 @@ static double EPS = 5e-15;
 double babel(double a, double x){
 	return .5*(x+a/x);
 }
+double babel2(double a, double x){
+	double x2 = x*x, x2a = x2+a;
+	return .25*(x2a*x2a+a*x2)/(x*x2a);
+}
 double abs(double x) {
 	return x>.0?x:-x;
 }
 double varSqrt(double jx, double jEps, int jMaxStep, double (*jf)(double,double)){
 	if(abs(jx-1.0)<EPS||abs(jx)<EPS) return jx;
-	double x = jx, eps = jEps>EPS?jEps:EPS;
+	double x = jx, cp = jx, eps = jEps>EPS?jEps:EPS;
 	int count = 0, max = jMaxStep>0?jMaxStep:-jMaxStep;
-	while(abs(x*x-jx)>eps&&count<max){
-		x = jf(jx,x);
+	while(abs(x*x-cp)>eps&&count<max){
+		x = jf(cp,x);
 		++count;
 	}
-	return .0;
+	return x;
 }
